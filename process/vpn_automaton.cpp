0a7d83153108fb0697c9fe0a964837187d7b7440
jirakja7 <jirakja7@fit.cvut.cz>
2022-12-15 11:26:32 +0100

    vpn_automaton implemented

--- process/vpn_automaton.cpp
+++ process/vpn_automaton.cpp
@@ -62,6 +62,7 @@ VPN_AUTOMATONPlugin::VPN_AUTOMATONPlugin()
 
 VPN_AUTOMATONPlugin::~VPN_AUTOMATONPlugin()
 {
+   close();
 }
 
 void VPN_AUTOMATONPlugin::init(const char *params)
@@ -77,28 +78,119 @@ ProcessPlugin *VPN_AUTOMATONPlugin::copy()
    return new VPN_AUTOMATONPlugin(*this);
 }
 
-int VPN_AUTOMATONPlugin::pre_create(Packet &pkt)
+inline void transition_from_init(RecordExtVPN_AUTOMATON *automaton_data, uint16_t len, const timeval& ts, uint8_t dir)
 {
-   return 0;
+   automaton_data->syn_table.update_entry(len, dir, ts);
+}
+
+inline void transition_from_syn(RecordExtVPN_AUTOMATON *automaton_data, uint16_t len, const timeval& ts, uint8_t dir)
+{
+   bool can_transit = automaton_data->syn_table.check_range_for_presence(len, 10, !dir, ts);
+   if (can_transit)
+   {
+      automaton_data->syn_ack_table.update_entry(len, dir, ts);
+   } 
+}
+
+inline bool transition_from_syn_ack(RecordExtVPN_AUTOMATON *automaton_data, uint16_t len, const timeval& ts, uint8_t dir)
+{
+   return automaton_data->syn_table.check_range_for_presence(len, 12, !dir, ts);
+}
+
+void VPN_AUTOMATONPlugin::update_record(RecordExtVPN_AUTOMATON *automaton_data, const Packet &pkt)
+{ 
+   /**
+    * 0 - client -> server
+    * 1 - server -> client
+    */
+   uint8_t dir = pkt.source_pkt ? 0 : 1;
+   uint16_t len = pkt.payload_len;
+   timeval ts = pkt.ts;
+
+   if ( !(MIN_PKT_SIZE <= len && len <= MAX_PKT_SIZE) ) return;
+
+   bool reached_end_state = transition_from_syn_ack(automaton_data, len, ts, dir);
+
+   if (reached_end_state) 
+   {
+      automaton_data->reset();
+      if (automaton_data->syn_pkts_idx < SYN_RECORDS_NUM)
+      {
+         automaton_data->syn_pkts[automaton_data->syn_pkts_idx] = len;
+         automaton_data->syn_pkts_idx += 1;
+      }
+      automaton_data->suspects += 1;
+      return;
+   }
+
+   transition_from_syn(automaton_data, len, ts, dir);
+   transition_from_init(automaton_data, len, ts, dir);
 }
 
 int VPN_AUTOMATONPlugin::post_create(Flow &rec, const Packet &pkt)
 {
-   return 0;
-}
+   RecordExtVPN_AUTOMATON *automaton_data = new RecordExtVPN_AUTOMATON();
+   rec.add_extension(automaton_data);
 
-int VPN_AUTOMATONPlugin::pre_update(Flow &rec, Packet &pkt)
-{
+   update_record(automaton_data, pkt);
    return 0;
 }
 
 int VPN_AUTOMATONPlugin::post_update(Flow &rec, const Packet &pkt)
 {
+   RecordExtVPN_AUTOMATON *automaton_data = (RecordExtVPN_AUTOMATON *) rec.get_extension(RecordExtVPN_AUTOMATON::REGISTERED_ID);
+   update_record(automaton_data, pkt);
    return 0;
+}
+
+double classes_ratio(uint8_t* syn_pkts, uint8_t size)
+{
+   uint8_t unique_members = 0;
+   bool marked[size];
+   for (uint8_t i = 0; i < size; ++i) marked[i] = false;
+   for (uint8_t i = 0; i < size; ++i)
+   {
+      if (marked[i]) continue;
+      uint8_t akt_pkt_size = syn_pkts[i];
+      unique_members++;
+      marked[i] = true;
+      for (uint8_t j = i + 1; j < size; ++j)
+      {
+         if (marked[j]) continue;
+         if (syn_pkts[j] == akt_pkt_size) marked[j] = true;
+      }
+   }
+
+   return double(unique_members) / double(size); 
 }
 
 void VPN_AUTOMATONPlugin::pre_export(Flow &rec)
 {
+   //do not export pstats for small packets flows
+   uint32_t packets = rec.src_packets + rec.dst_packets;
+   if (packets <= 30) {
+      rec.remove_extension(RecordExtVPN_AUTOMATON::REGISTERED_ID);
+      return;
+   }
+
+   RecordExtVPN_AUTOMATON *automaton_data = (RecordExtVPN_AUTOMATON *) rec.get_extension(RecordExtVPN_AUTOMATON::REGISTERED_ID);
+   const auto& suspects = automaton_data->suspects; 
+   if (suspects < 3) return;
+   if (double(packets)/double(suspects) > 2500) return;
+   if (suspects < 15)
+   {
+      if (classes_ratio(automaton_data->syn_pkts, automaton_data->syn_pkts_idx) > 0.6) return;
+   } 
+   else if (suspects < 40)
+   {
+      if (classes_ratio(automaton_data->syn_pkts, automaton_data->syn_pkts_idx) > 0.4) return;
+   }
+   else
+   {
+      if (classes_ratio(automaton_data->syn_pkts, automaton_data->syn_pkts_idx) > 0.2) return;
+   }
+
+   automaton_data->possible_vpn = 1;
 }
 
 }
--- process/vpn_automaton.hpp
+++ process/vpn_automaton.hpp
@@ -45,6 +45,8 @@
 #define IPXP_PROCESS_VPN_AUTOMATON_HPP
 
 #include <cstring>
+#include <sstream>
+#include <cassert>
 
 #ifdef WITH_NEMEA
   #include "fields.h"
@@ -63,17 +65,130 @@ UR_FIELDS (
    uint8 VPN_AUTOMATON_CONF_LEVEL
 )
 
+
+#define SYN_RECORDS_NUM 100
+#define PKT_TABLE_SIZE 91
+#define MIN_PKT_SIZE 60
+#define MAX_PKT_SIZE 150
+#define MAX_TIME_WINDOW 3000000 // in microseconds
+
+using dir_t = uint8_t;
+
+struct pkt_entry 
+{
+   timeval ts_dir1;
+   timeval ts_dir2;
+
+   void reset()
+   {
+      ts_dir1.tv_sec = 0;
+      ts_dir1.tv_usec = 0;
+      ts_dir2.tv_sec = 0;
+      ts_dir2.tv_usec = 0;
+   }
+
+   timeval& get_time(dir_t dir)
+   {
+      if (dir == 1)
+      {
+         return ts_dir1;
+      } 
+      else 
+      {
+         return ts_dir2;
+      }
+   }
+
+   pkt_entry()
+   {
+      reset();
+   }
+};
+
+struct pkt_table
+{
+   pkt_entry table_[PKT_TABLE_SIZE];
+
+   void reset()
+   {
+      for (int i = 0; i < PKT_TABLE_SIZE; ++i)
+      {
+         table_[i].reset();
+      }
+   }
+
+   bool check_range_for_presence(uint16_t len, uint8_t down_by, dir_t dir, const timeval& ts_to_compare)
+   {
+      int8_t idx = get_idx_from_len(len);
+      // if ( !(0 <= idx && idx < PKT_TABLE_SIZE) ) std::cout << "out:" << (int)idx << std::endl;
+      for (int8_t i = std::max(idx - down_by, 0); i <= idx; ++i)
+      {
+         if (entry_is_present(i, dir, ts_to_compare)) return true;
+      }
+      return false;
+   }
+
+   void update_entry(uint16_t len, dir_t dir, timeval ts)
+   {
+      int8_t idx = get_idx_from_len(len);
+      // if ( !(0 <= idx && idx < PKT_TABLE_SIZE) ) std::cout << "out:" << (int)idx << std::endl;
+      if (dir == 1)
+      {
+         table_[idx].ts_dir1 = ts;
+      } 
+      else 
+      {
+         table_[idx].ts_dir2 = ts;
+      }
+   }
+
+   private:
+   inline int8_t get_idx_from_len(uint16_t len)
+   {
+      return std::max(int(len) - MIN_PKT_SIZE, 0);
+   }
+
+   inline bool time_in_window(const timeval& ts_now, const timeval& ts_old)
+   {
+      long diff_secs = ts_now.tv_sec - ts_old.tv_sec;
+      long diff_micro_secs = ts_now.tv_usec - ts_old.tv_usec;
+
+      diff_micro_secs += diff_secs * 1000000;
+      if (diff_micro_secs > MAX_TIME_WINDOW) return false;
+      return true;
+   }
+
+   inline bool entry_is_present(int8_t idx, dir_t dir, const timeval& ts_to_compare)
+   {
+      timeval& ts = table_[idx].get_time(dir);
+      if (time_in_window(ts_to_compare, ts))
+      {
+         return true;
+      } 
+      return false;
+   }
+};
+
 /**
  * \brief Flow record extension header for storing parsed VPN_AUTOMATON data.
  */
 struct RecordExtVPN_AUTOMATON : public RecordExt {
    static int REGISTERED_ID;
 
-   uint8_t possible_vpn;
+   uint8_t possible_vpn {0}; // fidelity of this flow beint vpn
+   uint64_t suspects {0};
+   uint8_t syn_pkts_idx {0};
+   uint8_t syn_pkts[SYN_RECORDS_NUM];
 
-   RecordExtVPN_AUTOMATON() : RecordExt(REGISTERED_ID)
+   pkt_table syn_table{};
+   pkt_table syn_ack_table{};
+
+   RecordExtVPN_AUTOMATON() : RecordExt(REGISTERED_ID){}
+
+   void reset ()
    {
-      possible_vpn = 0;
+      syn_table.reset();
+      syn_ack_table.reset();
    }
 
 #ifdef WITH_NEMEA
@@ -105,6 +220,13 @@ struct RecordExtVPN_AUTOMATON : public RecordExt {
       };
       return ipfix_template;
    }
+
+   std::string get_text() const 
+   {
+      std::ostringstream out; 
+      out << "Result:" << (int)possible_vpn << std::endl; 
+      return out.str();
+   }
 };
 
 /**
@@ -122,9 +244,8 @@ public:
    RecordExt *get_ext() const { return new RecordExtVPN_AUTOMATON(); }
    ProcessPlugin *copy();
 
-   int pre_create(Packet &pkt);
+   void update_record(RecordExtVPN_AUTOMATON *automaton_data, const Packet &pkt);
    int post_create(Flow &rec, const Packet &pkt);
-   int pre_update(Flow &rec, Packet &pkt);
    int post_update(Flow &rec, const Packet &pkt);
    void pre_export(Flow &rec);
 };
